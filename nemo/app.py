09d22e5afe0f8451243248668aa7e6482e2580bb
karan100010 <karanjoshi1993@gmail.com>
2023-11-19 15:49:14 +0530

    download function added

--- nemo/app.py
+++ nemo/app.py
@@ -9,17 +9,37 @@ asr_model_hi = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name="stt
 
 # Load the English ASR model
 asr_model_en = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained("nvidia/stt_en_conformer_transducer_xlarge")
+import requests
+from pydub import AudioSegment
+from io import BytesIO
 
+def load_audio_from_url(url):
+    # Make a GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Get the audio content from the response
+        audio_content = BytesIO(response.content)
+
+        # Use pydub to load the audio
+        audio = AudioSegment.from_file(audio_content)
+
+        return audio
+    else:
+        print(f"Failed to fetch audio from {url}. Status code: {response.status_code}")
+        return None
+    
 @app.route('/transcribe_hi', methods=['POST'])
 def transcribe_hi():
     if 'audiofile_path' not in request.form:
         return jsonify({'error': 'No audio file path provided'}), 400
 
-    audiofile_path = request.form['audiofile_path']
+    audiofile =load_audio_from_url(request.form['audiofile'])
     
     try:
         # Transcribe the Hindi audio file
-        transcription = asr_model_hi.transcribe([audiofile_path])
+        transcription = asr_model_hi.transcribe([audiofile])
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
