6caa1337eefbb0ea973c8337d6454e2ff5a461d4
karan100010 <karanjoshi1993@gmail.com>
2024-01-14 23:25:52 +0530

    added

--- nemo/app.py
+++ nemo/app.py
@@ -93,11 +93,11 @@ import json
 def convert_file(file):
     # Decode and combine u-law fragments into a single bytearray
     combined_pcm_data = bytearray()
-    ulaw_data = bytes(file['data']['data'])
+   # ulaw_data = bytes(file['data']['data'])
 
 
     # Decode the u-law data to 16-bit linear PCM
-    pcm_data = audioop.ulaw2lin(ulaw_data, 2)
+    pcm_data = audioop.ulaw2lin(file, 2)
 
 
     # Save the combined PCM data to a WAV file
