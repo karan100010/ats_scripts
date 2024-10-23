788702818977c357896803d4ae83d3fd13bbb03e
karan100010 <karanjoshi1993@gmail.com>
2024-10-23 16:42:47 +0530

    added

--- phi3/app.py
+++ phi3/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import lmdeploy
-from lmdeploy.model import Model
-from lmdeploy.sampler import Sampler
+from lmdeploy import LM
 import torch
 
 # Initialize Flask app
@@ -11,16 +10,13 @@ app = Flask(__name__)
 model_path = "microsoft/Phi-3-mini-4k-instruct"
 
 # Initialize model with lmdeploy
-model = Model(
+model = LM(
     model_path=model_path,
     use_quantization=True,  # Enable quantization, if needed
     quantization_method="awq",  # Quantization method
     dtype="fp16",  # Data type
     gpu_memory_utilization=0.5  # GPU memory utilization
 )
-
-# Set up a Sampler instance
-sampler = Sampler(model=model, temperature=0, max_tokens=64)
 
 @app.route('/generate_text', methods=['POST'])
 def generate_text():
@@ -36,11 +32,11 @@ def generate_text():
         # Prepare the prompt
         prompt = f"<|system|>you are a call center operator. And you will give reply in json with the following parameters intent, language, appropriate answer and next step \n<|user|>\n{input_text}<|end|>\n<|assistant|>\n"
         
-        # Generate outputs using the sampler
-        generated_output = sampler.generate(prompt)
+        # Generate outputs using the model
+        generated_output = model.generate(prompt, temperature=0, max_tokens=64)
         
         # Collect and return the generated text
-        generated_text = generated_output.text
+        generated_text = generated_output[0]
         return jsonify({"generated_texts": [generated_text]})
     
     except Exception as e:
@@ -49,3 +45,4 @@ def generate_text():
 # Run the Flask app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
+
