43036d090ecd6cb310db0773917dad32cd1578c7
karan100010 <karanjoshi1993@gmail.com>
2024-11-21 23:18:14 +0530

    added

--- prompt_api/app.py
+++ prompt_api/app.py
@@ -143,10 +143,10 @@ bot2_instructions = """You are a clinet taking to recovery agent. Speak in short
 
 
 
-bradman_bot = ConversationAgent(model, "Vinod", bot_instructions, context_turns=1)
-jardine_bot = ConversationAgent(model, "Sumit", bot2_instructions, context_turns=1)
+bradman_bot = ConversationAgent(model, "Vinod", bot_instructions, context_turns=5)
+jardine_bot = ConversationAgent(model, "Sumit", bot2_instructions, context_turns=5)
 
-conversation_turns = conversation_simulator(bradman_bot, jardine_bot, total_turns=1)
+conversation_turns = conversation_simulator(bradman_bot, jardine_bot, total_turns=3)
 
 for turn in conversation_turns:
     print(f"{turn['name']}: {turn['text']}\n")
