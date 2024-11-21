6a70505a2b324a204b19fd5f86edb2739f71b561
karan100010 <karanjoshi1993@gmail.com>
2024-11-22 01:51:36 +0530

    added

--- prompt_api/api.py
+++ prompt_api/api.py
@@ -4,11 +4,35 @@ import re
 from guidance import gen, select, system, user, assistant,models
 
 app = Flask(__name__)
-
+model=models.Transformers("microsoft/Phi-3.5-mini-instruct")
 
 @app.route('/get_reply', methods=['POST'])
 def genrate_next():
-    
+    messages = request['messages']
+
+# Check if the first message is from 'system' role
+    if messages and messages[0]['role'] == 'system':
+        system_message = messages[0]['content']
+        # Write the system prompt at the top
+        with system():
+            model += f"""{system_message}"""
+    role_messages = {}
+    for message in parsed_data['messages']:
+        role = message.get('role')
+        if role not in role_messages:
+            role_messages[role] = []
+        role_messages[role].append(message['content'])
+        for message in messages:
+            role = message['role']
+            content = message['content']
+            if role == 'user':
+                with user():
+                    model += f"""{content}
+        """
+            elif role == 'assistant':
+                with assistant():
+                    model += f"""{content}
+"""
 
 
 
