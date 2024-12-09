a158a7c0976d84da89f658bc7e4d65bd3ebd412c
karan100010 <karanjoshi1993@gmail.com>
2024-12-09 13:37:20 +0530

    added

--- nemo-en/app.py
+++ nemo-en/app.py
@@ -85,8 +85,8 @@ def transcribe_hi():
     
     try:
         # Transcribe the Hindi audio file
-            with torch.cuda.amp.autocast():
-                 transcription= asr_model_hi.transcribe([request.form['audiofile']],batch_size=128)[0]
+          
+                 transcription= asr_model_hi.transcribe([request.form['audiofile']],batch_size=32)[0]
         # = asr_model_en.transcribe([request.form['audiofile']],batch_size=128)
 
     except Exception as e:
