4efe4dfe52b95a6729daeafbbd9264c4e7e9a521
karan100010 <karanjoshi1993@gmail.com>
2024-12-10 14:37:09 +0530

    added

--- nemo-en/vosk/app.py
+++ nemo-en/vosk/app.py
@@ -16,14 +16,14 @@ asr_model_en=vosk.Model("/fra/KARAN/vosk-model-en-in-0.5/vosk-model-en-in-0.5")
 
 @app.route('/vosk_hi', methods=['POST'])
 
-def english():
+def hindi():
      recognizer_hi = recognizer_hi.AcceptWaveform(request.get_data())
      result = recognizer_hi.FinalResult()
      return jsonify(result)
 
 @app.route('/vosk_en', methods=['POST'])
 
-def hindi():
+def english():
      recognizer_en = vosk.KaldiRecognizer(asr_model_en, 8000)
      recognizer_en.AcceptWaveform(request.get_data())
      result = recognizer_en.FinalResult()
